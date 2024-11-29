a25c45b46c8b528d231916266cbb22d175aafdc1
Maycon Rodrigues <maycon.zng@gmail.com>
2024-11-29 10:05:17 -0300

    Add wallet modal button

--- components/main-interface.tsx
+++ components/main-interface.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { formatWallerAddresse } from "@/lib/utils"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { NetworkSelector } from "@/components/network-selector";
 import { ContractInvoker } from "@/components/contract-invoker";
@@ -9,18 +10,43 @@ import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 import { Rocket, Wallet } from "lucide-react";
 import { useToast } from "@/components/ui/use-toast";
+import useStellarWalletsKit from "@/hooks/useStellarWalletKit";
+import { ISupportedWallet } from "@creit.tech/stellar-wallets-kit";
 
 export function MainInterface() {
   const [network, setNetwork] = useState("testnet");
+  const [address, setAddress] = useState<string>("");
   const { toast } = useToast();
+  const kit = useStellarWalletsKit();
+
 
   const handleConnectWallet = async () => {
     try {
       // Here we would implement actual wallet connection logic
-      toast({
-        title: "Wallet Connected",
-        description: "Successfully connected to your Stellar wallet",
+      await kit.openModal({
+        onWalletSelected: async (option: ISupportedWallet) => {
+          kit.setWallet(option.id);
+          const { address } = await kit.getAddress();
+          // Do something else
+          console.log("Address: ", address);
+          setAddress(address);
+
+          if (address.length > 0) {
+            toast({
+              title: "Wallet Connected",
+              description: "Successfully connected to your Stellar wallet",
+            });
+          }
+        },
+        onClosed: (error: Error) => {
+          toast({
+            title: "Connection Failed",
+            description: `Failed to connect to wallet ${error}`,
+            variant: "destructive",
+          });
+        }
       });
+
     } catch (error) {
       toast({
         title: "Connection Failed",
@@ -45,7 +71,7 @@ export function MainInterface() {
             className="flex items-center gap-2"
           >
             <Wallet className="h-4 w-4" />
-            Connect Wallet
+            {address ? formatWallerAddresse(address) : "Connect Wallet"}
           </Button>
           <ThemeToggle />
         </div>
